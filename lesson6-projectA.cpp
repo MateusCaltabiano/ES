68eaa8de4113ba04115405a76d29f94c2b2472e7
Mateus Caltabiano <87660354+MateusCaltabiano@users.noreply.github.com>
2023-11-21 03:25:49 -0300

    Create lesson6-projectA.cpp

---
+++ lesson6-projectA.cpp
@@ -0,0 +1,18 @@
+void setup()
+{
+  pinMode(2, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(2), on_push, FALLING);
+  Serial.begin(9600);
+}
+
+void on_push()
+{
+  Serial.println(digitalRead(13));
+  digitalWrite(13, !digitalRead(13));
+}
+
+void loop()
+{
+  delay(1000);
+}
