0113426985085d694c1b9fb9753461a139e3772c
Mateus Caltabiano <87660354+MateusCaltabiano@users.noreply.github.com>
2023-11-21 03:24:48 -0300

    Create Design-6B.cpp

---
+++ Design-6B.cpp
@@ -0,0 +1,39 @@
+#include <LiquidCrystal.h>
+
+#define RS 9
+#define EN 8
+#define DB4 4
+#define DB5 5
+#define DB6 6
+#define DB7 7
+
+LiquidCrystal lcd(RS, EN, DB4, DB5, DB6, DB7);
+
+void setup()
+{
+ Serial.begin(9600);
+ lcd.begin(16, 2);
+ lcd.clear();
+}
+
+void setLcd(int temperature)
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(String(temperature) + " C");
+} 
+  
+
+void loop()
+{
+  int reading, temp;
+  float voltage;
+  reading = analogRead(A0);
+  voltage = reading * 5.0/1024.0;
+  temp = (voltage-0.5)*100;
+  Serial.println(temp);
+  setLcd(temp);
+  lcd.setCursor(0, 1);
+  lcd.print(String(millis() / 1000) + " seconds");
+  delay(100);
+}
