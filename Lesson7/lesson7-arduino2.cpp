0dcea701baeef0504a719bbce2250928142d7629
Mateus Caltabiano <87660354+MateusCaltabiano@users.noreply.github.com>
2024-01-06 16:03:08 +0900

    Create lesson7-arduino2.cpp

---
+++ Lesson7/lesson7-arduino2.cpp
@@ -0,0 +1,39 @@
+#define LED_C 13
+#define LED_D 12
+#define INT_IN 2
+#define DATA_IN 3
+
+int led = 1;
+
+void setup() 
+{
+ pinMode(LED_C, OUTPUT);
+ pinMode(LED_D, OUTPUT);
+ pinMode(DATA_IN, INPUT_PULLUP);
+ pinMode(INT_IN, INPUT_PULLUP);
+ attachInterrupt(digitalPinToInterrupt(INT_IN), on_data, FALLING);
+ update_led();
+}
+
+void on_data()
+{
+ int s;
+ led >>= 1;
+ led &= 0x03;
+ s = digitalRead(DATA_IN);
+ if (s == HIGH) {
+ 	led |= 2;
+ }
+ update_led();
+}
+
+void update_led()
+{
+ digitalWrite(LED_C, (led >> 1) & 1 ? HIGH : LOW);
+ digitalWrite(LED_D, (led >> 0) & 1 ? HIGH : LOW);
+}
+
+void loop()
+{
+ delay(1000); 
+}
